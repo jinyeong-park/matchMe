94ab8b346516a74d8e66a31e060d5e2b1a45419a
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-01-08 18:20:17 -0800

    Create Header component:

--- dating-app/src/App.js
+++ dating-app/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Match Me</h1>
+      <Header />
+      {/* Card */}
 
     </div>
   );
